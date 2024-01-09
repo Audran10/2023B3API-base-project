4f4cac068266702a0cd3ced46f3a48560fd2e26a
ALATORRE <audran.latorre@ynov.com>
2024-01-09 09:02:48 +0100

    logger

--- src/middleware/request-logger.middleware.ts
+++ src/middleware/request-logger.middleware.ts
@@ -6,15 +6,13 @@ import * as fs from 'fs';
 export class RequestLoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const ip = req.ip || req.connection.remoteAddress;
+    const method = req.method;
     const route = req.originalUrl;
-    const params = JSON.stringify(req.params);
     const dateTime = new Date().toISOString();
 
-    // Log the request details to logs.txt
-    const logMessage = `${ip} ${dateTime} - Route: ${route} - Params: ${params}\n`;
+    const logMessage = `${ip} ${dateTime} ${method} - ${route}\n`;
     fs.appendFileSync('logs.txt', logMessage);
 
-    // Continue processing the request
     next();
   }
 }
